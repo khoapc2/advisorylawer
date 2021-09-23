a577bf2c5c44da7e63a860be149bc24ad2cefacc
quoclinh1201 <quoclinh1212000@gmail.com>
2021-09-23 17:26:28 +0700

    Implement GenericRepository

---
+++ AdvisoryLawyer/AdvisoryLawyer.Data/IRepositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AdvisoryLawyer.Data.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ AdvisoryLawyer/AdvisoryLawyer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using AdvisoryLawyer.Data.IRepositories;
+using AdvisoryLawyer.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvisoryLawyer.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private AdvisoryLawyerContext _context = null;
+        private DbSet<T> _dbSet = null;
+
+        public GenericRepository(AdvisoryLawyerContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            _dbSet.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            _dbSet.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            T existing = _dbSet.Find(id);
+            _dbSet.Remove(existing);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
