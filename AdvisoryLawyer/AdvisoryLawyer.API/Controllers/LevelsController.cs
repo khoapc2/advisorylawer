f64798b1e1e809553876f2c9f0d374f8307d93e5
quoclinh1201 <quoclinh1212000@gmail.com>
2021-09-25 23:16:23 +0700

    Implement CRUD for Level API

---
+++ AdvisoryLawyer/AdvisoryLawyer.API/Controllers/LevelsController.cs
@@ -0,0 +1,112 @@
+﻿using AdvisoryLawyer.Business.IServices;
+using AdvisoryLawyer.Business.Requests.LevelRequest;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvisoryLawyer.API.Controllers
+{
+    [Route("api/levels")]
+    [ApiController]
+    public class LevelsController : ControllerBase
+    {
+        private readonly ILevelService _service;
+
+        public LevelsController(ILevelService service)
+        {
+            _service = service;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAllLevels()
+        {
+            try
+            {
+                var levelList = _service.GetAllLevels();
+                if(levelList != null)
+                {
+                    return Ok(levelList);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public IActionResult GetLevelByID(int id)
+        {
+            try
+            {
+                var level = _service.GetLevelByID(id);
+                if (level != null) return Ok(level);
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult CreateLevel([FromBody] LevelRequest newLevel)
+        {
+            try
+            {
+                var level = _service.CreateLevel(newLevel);
+                if(level != null) return Ok();
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public IActionResult UpdateLevel(int id, [FromBody] LevelRequest newLevel)
+        {
+            try
+            {
+                var level = _service.UpdateLevel(id, newLevel);
+                if (level != null) return Ok();
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLevel(int id)
+        {
+            try
+            {
+                bool isDelete = _service.DeleteLevel(id);
+                if (isDelete) return Ok();
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ AdvisoryLawyer/AdvisoryLawyer.Business/Services/LevelService.cs
@@ -0,0 +1,106 @@
+﻿using AdvisoryLawyer.Business.IServices;
+using AdvisoryLawyer.Business.Requests.LevelRequest;
+using AdvisoryLawyer.Business.ViewModel;
+using AdvisoryLawyer.Data.IRepositories;
+using AdvisoryLawyer.Data.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvisoryLawyer.Business.Services
+{
+    public class LevelService : ILevelService
+    {
+        private readonly IGenericRepository<Level> _genericRepository;
+        private readonly IMapper _mapper;
+
+        public LevelService(IGenericRepository<Level> genericRepository, IMapper mapper)
+        {
+            _genericRepository = genericRepository;
+            _mapper = mapper;
+        }
+
+        public LevelModel CreateLevel(LevelRequest level)
+        {
+            try
+            {
+                var newLevel = _mapper.Map<Level>(level);
+                _genericRepository.Insert(newLevel);
+                _genericRepository.Save();
+                return _mapper.Map<LevelModel>(newLevel);
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return null;
+            }
+        }
+
+        public bool DeleteLevel(int id)
+        {
+            try
+            {
+                _genericRepository.Delete(id);
+                _genericRepository.Save();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return false;
+            }
+        }
+
+        public IEnumerable<LevelModel> GetAllLevels()
+        {
+            try
+            {
+                var levelList = _genericRepository.GetAll();
+                if(levelList != null)
+                {
+                    return _mapper.Map<IEnumerable<LevelModel>>(levelList).ToList();
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return null;
+            }
+        }
+
+        public LevelModel GetLevelByID(int id)
+        {
+            try
+            {
+                var level = _genericRepository.GetByID(id);
+                return _mapper.Map<LevelModel>(level);
+            }
+            catch (Exception ex) 
+            {
+                //logging
+                return null;
+            }
+        }
+
+        public LevelModel UpdateLevel(int id, LevelRequest level)
+        {
+            try
+            {
+                var newLevel = _mapper.Map<Level>(level);
+                newLevel.Id = id;
+                _genericRepository.Update(newLevel);
+                _genericRepository.Save();
+                return _mapper.Map<LevelModel>(newLevel);
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return null;
+            }
+        }
+    }
+}
