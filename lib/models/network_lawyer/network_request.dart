bf250ac5fdd39d8821b61bb0d9fa918904d7d000
duongdtse141129 <duong97498@gmail.com>
2021-11-09 07:01:06 +0700

    second

--- lib/models/network_lawyer/network_request.dart
+++ lib/models/network_lawyer/network_request.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:advisories_lawyer/lawyer/infor_user.dart';
 import 'package:advisories_lawyer/lawyer/model_lawyer/category.dart';
 import 'package:advisories_lawyer/lawyer/model_lawyer/document.dart';
 import 'package:advisories_lawyer/lawyer/model_lawyer/post.dart';
 import 'package:advisories_lawyer/lawyer/model_lawyer/slot.dart';
+import 'package:advisories_lawyer/models/booking.dart';
+
+import 'package:advisories_lawyer/models/customer_case.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:http/http.dart' as http;
 
-class NetworkRequest {
+class NetworkRequest{
   static const String url = 'https://jsonplaceholder.typicode.com/posts';
   static const String urlCategory =
       'https://104.215.186.78/api/v1/categories?page_index=1&page_size=6';
@@ -111,6 +115,136 @@ class NetworkRequest {
       throw Exception('Failed to get post');
     }
   }
+
+  static Future<CustomerCase> createCustomerCase(CustomerCase cusCase) async {
+      print("CCCsssss cuscase :"+ cusCase.name +"---"+cusCase.description);
+      final response = await http.post(
+        Uri.parse('https://104.215.186.78/api/v1/customer-cases'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(<String, dynamic>{
+          "name": cusCase.name,
+          "description": cusCase.description,
+        }),
+      );
+      print("CCC cuscase :"+response.body);
+      Map<String, dynamic> cusCaseJSON = jsonDecode(response.body);
+      CustomerCase cusCaseDTO = CustomerCase.fromJson(cusCaseJSON);
+      print("CCC cuscase2 :"+cusCaseDTO.toString());
+      print("R cuscase :"+response.statusCode.toString());
+      if (response.statusCode == 201) {
+        print("create CusCase dc r");
+        return cusCaseDTO;
+      } else {
+        throw Exception('Failed to create slot.');
+      }
+    
+  }
+
+
+
+
+  static Future<CustomerCase> fetachCustomerCase(CustomerCase cusCase,{int page = 1}) async {
+    String name=cusCase.name.replaceAll(" ", "%20");
+    String description=cusCase.description.replaceAll(" ", "%20");
+
+    final response = await http.get(Uri.parse('https://104.215.186.78/api/v1/customer-cases?name=${name}&description=${description}&page_index=1&page_size=100'));
+
+    Map<String, dynamic> cusCaseMap = jsonDecode(response.body);
+    CustomerCase customerCaseDTO = CustomerCase.fromJson(cusCaseMap);
+
+    if (response.statusCode == 200) {
+      print("connect dc r" +response.body);
+      return customerCaseDTO;
+    } else if (response.statusCode == 404) {
+      print("Not found ");
+      throw Exception('Not Found');
+    } else {
+      print("Fail to connect");
+      throw Exception('Failed to get post');
+    }
+  }
+
+  static Future<Booking> createBooking(Booking dto) async {
+    
+      final response = await http.post(
+        Uri.parse('https://104.215.186.78/api/v1/bookings'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(<String, dynamic>{
+          "customer_id": InforUser.getIdUser(),
+          "lawyer_id": dto.lawyerId,
+          "booking_date": dto.bookingDate,
+          "payment_method": dto.paymentMethod,
+          "total_price": dto.totalPrice,
+          "pay_date": dto.payDate,
+          "status": 1,
+          "customer_case_id": dto.customerCaseId
+        }),
+      );
+      //https://104.215.186.78/api/v1/bookings/20
+
+      Map<String, dynamic> bookingJSON = jsonDecode(response.body);
+      print(response.body.toString());
+      Booking bookingDTO = Booking.fromJson(bookingJSON);
+      print("R:"+response.statusCode.toString());
+      if (response.statusCode == 201) {
+        print("create Booking dc r");
+        return bookingDTO;
+      } else {
+        throw Exception('Failed to create slot.');
+      }
+    
+  }
+
+  static Future<Booking> getBookingID(int bookingID) async {
+    
+      final response = await http.get(Uri.parse('https://104.215.186.78/api/v1/bookings/$bookingID'));
+
+      Map<String, dynamic> bookingJSON = jsonDecode(response.body);
+      print(response.body.toString());
+      Booking bookingDTO = Booking.fromJson(bookingJSON);
+      print("R:"+response.statusCode.toString());
+      if (response.statusCode == 200) {
+        print("create CusCase dc r");
+        return bookingDTO;
+      } else {
+        throw Exception('Failed to create slot.');
+      }
+    
+  }
+
+  static Future<void> updateSlot(int idSlot,int bookingID) async {
+    print(idSlot.toString()+ "- --- -"+bookingID.toString());
+    var idToken = await FirebaseAuth.instance.currentUser!.getIdToken();
+    
+      final response = await http.put(Uri.parse('https://104.215.186.78/api/v1/slots'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $idToken',
+        },
+        body: jsonEncode(<String, int>{
+          "id": idSlot,
+          "booking_id": bookingID
+        }),
+      );
+      String body = response.body;
+      print(body.toString()+"||");
+
+      print("R:"+response.statusCode.toString());
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        print("update booking to slot dc r");
+      } else {
+        throw Exception('Failed to connect.');
+      }
+    
+  }
+
 
   /*Future<List<Post>> fetchProducts() async { 
    final response = await http.get('http://192.168.1.2:8000/products.json' as Uri); 
